3f08b10a5fffa5b69833226a5087745c6c03e7a4
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-09-01 04:49:21 +0300

    fffiiixxx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from 'react-hot-toast';
 import { NavLink, Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
-import MovieInfo from './MovieInfo/MovieInfo';
+import MovieDetails from 'pages/MovieDetails';
 
 export const App = () => {
   return (
@@ -14,7 +14,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<div>Movies</div>} />
-        <Route path="/movies/:movieId" element={<MovieInfo />}>
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
           <Route path="/movies/:movieId/cast" element={<div>Cast</div>} />
           <Route path="/movies/:movieId/reviews" element={<div>Reviews</div>} />
         </Route>
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,110 @@
+import { RiArrowLeftCircleFill } from 'react-icons/ri';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { fetchMovieInfo } from 'services/api';
+import { toast } from 'react-hot-toast';
+
+const MovieDetails = () => {
+  const { movieId } = useParams();
+  const [info, setInfo] = useState(null);
+
+  useEffect(() => {
+    const route = `/movie/${movieId}`;
+    toast.loading('Wait a minute...', { duration: 700 });
+
+    setTimeout(async () => {
+      try {
+        const movieDetails = await fetchMovieInfo(route);
+        setInfo(movieDetails);
+      } catch (error) {
+        console.warn(error);
+        toast.error('Oops! Something went wrong...');
+      } finally {
+        toast.success('Successfully fetched!');
+      }
+    }, 800);
+  }, [movieId]);
+
+  if (info === null) {
+    return;
+  }
+
+  const {
+    title,
+    original_title,
+    poster_path,
+    budget,
+    genres,
+    homepage,
+    release_date,
+    vote_average,
+    overview,
+  } = info;
+  const poster = poster_path && `http://image.tmdb.org/t/p/w342${poster_path}`;
+  const release = release_date && release_date.slice(0, 4);
+  const rating = vote_average && Math.round(vote_average * 10);
+  const tags =
+    genres &&
+    genres.map(tag => (
+      <li key={tag.id} style={{ marginRight: '22px' }}>
+        <p>{tag.name}</p>
+      </li>
+    ));
+
+  return (
+    <>
+      {info && (
+        <main style={{ padding: '8px' }}>
+          <hr />
+          <Link
+            to={'/'}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              width: 'fit-content',
+              border: '1px solid',
+              padding: '4px',
+              marginBottom: '2px',
+              textDecoration: 'none',
+              backgroundColor: 'lightgrey',
+            }}
+          >
+            <RiArrowLeftCircleFill /> Go back
+          </Link>
+          <section style={{ display: 'flex' }}>
+            <img src={poster} alt={original_title} />
+            <div style={{ marginLeft: '22px' }}>
+              <h2>
+                {title} ({release})
+              </h2>
+              {budget ? (
+                <p>
+                  The budget($): <b>{budget}</b>
+                </p>
+              ) : null}
+              <p>
+                User Score: <b>{rating}%</b>
+              </p>
+              <h3>Overview</h3>
+              <p>{overview} </p>
+              <h4>Genres</h4>
+              <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+                {tags}
+              </ul>
+              <p>Homepage: </p>
+              <a href={homepage} target="_blank" rel="noreferrer">
+                {homepage}
+              </a>
+            </div>
+          </section>
+          <hr />
+          <section>
+            <h5>Additional information</h5>
+          </section>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default MovieDetails;
