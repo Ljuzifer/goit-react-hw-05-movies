bca3c7d6cf5fc838731408ba4980dd60c4683192
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-09-01 16:43:29 +0300

    working....

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,16 +1,62 @@
+import { useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { useSearchParams } from 'react-router-dom';
+import { fetchMovieInfo } from 'services/api';
+
 const MoviesPage = () => {
+  const [searchQuery, setSearchQuery] = useSearchParams();
+  const currentSearch = searchQuery.get('search') ?? '';
+  console.log(currentSearch);
+
+  const onChangeSearch = e => {
+    e.preventDefault();
+    const newSearch = e.target.elements.search.value;
+
+    if (newSearch === '') {
+      toast.error('You must enter some text...');
+      return;
+    }
+    setSearchQuery({ search: newSearch });
+    e.target.elements.search.value = '';
+  };
+
+  useEffect(() => {
+    if (currentSearch === '') {
+      return;
+    }
+    // /search/movie?query=hitm
+    const some = `&query=${currentSearch}`;
+    const route = '/search/movie';
+
+    setTimeout(async () => {
+      try {
+        const { results, total_results } = await fetchMovieInfo(route, some);
+        console.log(total_results);
+      } catch (error) {
+        toast.error('Oops!');
+      }
+    }, 800);
+  }, [currentSearch]);
+
   return (
     <main>
-      <h2>Movie search</h2>
-      <form>
+      {currentSearch === '' && <h2>Ok! Let's find some movie...</h2>}
+      <form onSubmit={onChangeSearch}>
         <input
           type="text"
+          name="search"
+          // value={currentSearch}
           autoComplete="off"
           autoFocus
           placeholder="Search movies"
+          // onChange={e => e.target.value}
         />
         <button type="submit">Go on</button>
       </form>
+      <hr />
+      <section>
+        <ul></ul>
+      </section>
     </main>
   );
 };
