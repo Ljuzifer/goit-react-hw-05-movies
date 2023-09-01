f47163d232ba2570f9a9b3ed66578bca0ed0d529
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-09-01 23:02:11 +0300

    cast

--- src/components/MoviesCast/MoviesCast.jsx
+++ src/components/MoviesCast/MoviesCast.jsx
@@ -1,15 +1,53 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+import { fetchMovieInfo } from 'services/api';
+
 const MoviesCast = () => {
+  const { movieId } = useParams();
+  const [details, setDetails] = useState([]);
+  // /movie/{movie_id}/credits
+
+  useEffect(() => {
+    const route = `/movie/${movieId}/credits`;
+    // toast.loading('Wait a minute...', { duration: 700 });
+
+    setTimeout(async () => {
+      try {
+        const { cast } = await fetchMovieInfo(route);
+        setDetails([...cast]);
+      } catch (error) {
+        console.warn(error);
+        toast.error('Oops! Something went wrong...');
+      } finally {
+        toast.success('You`ve got it!');
+      }
+    }, 400);
+  }, [movieId]);
+
   return (
     <section>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quia
-        optio pariatur fugiat iusto, similique sequi quam magni voluptas
-        voluptates, est molestias repellat alias facilis, quaerat autem eveniet
-        aliquam. Recusandae. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Vel quibusdam sequi eligendi quis corporis consequatur cumque
-        accusamus, eum ex ipsam expedita, esse ipsa veniam, dolorum voluptatem
-        aspernatur laborum maxime. Cum.
-      </p>
+      <ul style={{ listStyle: 'none' }}>
+        {details !== [] &&
+          details.map(info => (
+            <li key={info.id}>
+              <img
+                src={
+                  info.profile_path &&
+                  `http://image.tmdb.org/t/p/w185${info.profile_path}`
+                }
+                alt={info.original_name}
+                // width="68"
+                // height="102"
+              />
+              <h3>{info.name} </h3>
+              <p>
+                Character: <b>{info.character} </b>
+              </p>
+              <hr />
+            </li>
+          ))}
+      </ul>
     </section>
   );
 };
