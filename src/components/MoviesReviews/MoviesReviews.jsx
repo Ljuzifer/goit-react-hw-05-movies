442edd38900d00c537a4d5d5edb97aa25d710a53
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-09-02 00:50:51 +0300

    xxx

--- src/components/MoviesReviews/MoviesReviews.jsx
+++ src/components/MoviesReviews/MoviesReviews.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import { fetchMovieInfo } from 'services/api';
+import { TbMoodCry } from 'react-icons/tb';
 
 const MoviesReviews = () => {
   const { movieId } = useParams();
@@ -14,18 +15,27 @@ const MoviesReviews = () => {
     setTimeout(async () => {
       try {
         const { results } = await fetchMovieInfo(route);
+        if (results?.length === 0) {
+          toast('Sorry, no reviews...');
+          return;
+        }
         setDetails([...results]);
+        toast.success('You`ve got it!');
       } catch (error) {
         console.warn(error);
         toast.error('Oops! Something went wrong...');
-      } finally {
-        toast.success('You`ve got it!');
       }
     }, 400);
   }, [movieId]);
 
   return (
     <section>
+      {details?.length === 0 && (
+        <b style={{ textAlign: 'center' }}>
+          Sorry, we don't have any reviews for this movies...
+          <TbMoodCry />
+        </b>
+      )}
       <ul style={{ listStyle: 'none' }}>
         {details.map(item => (
           <li key={item.id}>
@@ -35,7 +45,7 @@ const MoviesReviews = () => {
             <span style={{ textDecoration: 'underline' }}>
               {item.created_at.slice(0, 10)}{' '}
             </span>
-            <p>{item.content} </p>
+            <p>{item.content}</p>
             <hr />
           </li>
         ))}
